dbef0d07a400b94e2fe18f6a7de7f358467bdaf4
stefanni Santos <stefanni@gmail.com>
2023-01-03 12:43:32 -0300

    First commit

---
+++ src/test/java/BookingTests.java
@@ -0,0 +1,111 @@
+import com.github.javafaker.Faker;
+import entities.Booking;
+import entities.BookingDates;
+import entities.User;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+
+public class BookingTests {
+
+    public static Faker faker;
+    private static RequestSpecification request;
+    private static Booking booking;
+    private static BookingDates bookingDates;
+    private static User user;
+
+    @BeforeAll
+
+    public static void setup(){
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        faker = new Faker();
+        user = new User(
+                faker.name().username(),
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.internet().safeEmailAddress(),
+                faker.internet().password(8,10),
+                faker.phoneNumber().toString());
+
+        bookingDates = new BookingDates("2018-01-02", "2018-01-03");
+        booking = new Booking(user.getFirstname(), user.getLastname(),
+                (float)faker.number().randomDouble(2, 50, 100000),
+                true,bookingDates,
+                "");
+        RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter(), new ErrorLoggingFilter());
+
+    }
+
+    @BeforeEach
+    void setRequest(){
+        request = given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .contentType(ContentType.JSON)
+                .auth().basic("admin", "password123");
+    }
+
+    @Test
+    public void getAllBookingsById_returnOk(){
+        Response response = request
+                .when()
+                .get("/booking")
+                .then()
+                .extract()
+                .response();
+
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(200, response.statusCode());
+    }
+
+    @Test
+    public void  getAllBookingsByUserFirstName_BookingExists_returnOk(){
+        request
+                .when()
+                .queryParam("firstName", "Carol")
+                .get("/booking")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .and()
+                .body("results", hasSize(greaterThan(0)));
+
+    }
+
+    @Test
+    public void  CreateBooking_WithValidData_returnOk(){
+
+        Booking test = booking;
+                request
+                .when()
+                .body(booking)
+                .post("/booking")
+                .then()
+                .body(matchesJsonSchemaInClasspath("RequestSchema.json"))
+                .and()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON).and().time(lessThan(2000L));
+
+
+
+    }
+
+
+
+}
